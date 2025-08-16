73f47c90b06f57f725202db53828862f70d4555d
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-16 14:44:18 +0600

    main home layout

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,10 @@
+import ClientsLayout from '@/layouts/ClientsLayout'
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <ClientsLayout>
+      <></>
+    </ClientsLayout>
+  )
+}
