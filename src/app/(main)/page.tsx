473a0588d4ebce8486ac73c4f3444e85e54927f6
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-16 17:58:14 +0600

    sections are ading

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,11 @@
+import HeroSection from '@/components/ui/section/HeroSection'
 import ClientsLayout from '@/layouts/ClientsLayout'
 import React from 'react'
 
 export default function HomePage() {
   return (
-    <ClientsLayout>
-      <></>
-    </ClientsLayout>
+    <div>
+      <HeroSection />
+    </div>
   )
 }
