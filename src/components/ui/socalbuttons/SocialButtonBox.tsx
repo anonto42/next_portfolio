a348054f92c7bf1760e827d9fa4927deb17b9333
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-17 09:09:56 +0600

    button box for the socal icons

---
+++ src/components/ui/socalbuttons/SocialButtonBox.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function SocialButtonBox({
+    elements
+}: Readonly<{
+    elements: {
+        href: string;
+        icon: React.ReactNode;
+        content: string
+    }[]
+}>) {
+  return (
+    <div className=' flex gap-2 mt-5 items-center justify-start'>
+
+        <button className="bg-[rgba(255,255,255,0.19)] 
+        border border-[rgba(255,255,255,0.64)] 
+        rounded-full 
+        shadow-[0_4px_30px_rgba(0,0,0,0.1)] 
+        backdrop-blur-[8.3px] 
+        h-[46px] w-[124px]
+        flex items-center justify-center 
+        hover:scale-110 cursor-pointer
+        transition-transform duration-300">Contact Me</button>
+
+        {
+
+            elements.map((item, index) => (
+                <a href={item.href} key={index}>
+                    <button className="
+                        bg-[rgba(255,255,255,0.19)] 
+                        border border-[rgba(255,255,255,0.64)] 
+                        rounded-full 
+                        shadow-[0_4px_30px_rgba(0,0,0,0.1)] 
+                        backdrop-blur-[8.3px] 
+                        w-[46px] h-[46px] 
+                        flex items-center justify-center 
+                        hover:scale-110 cursor-pointer
+                        transition-transform duration-300
+                        ">
+                        {item.icon}
+                    </button>
+                </a>
+            ))
+        }
+        
+    </div>
+  )
+}
