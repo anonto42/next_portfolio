118319884994b1e742f8d93dcc53fd759c3db97e
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-17 10:23:10 +0600

    Image box for hero section

---
+++ src/components/ui/section/ImageBoxUnderHeroSection.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import React from 'react'
+import HeroImageButton from '../Buttons/HeroImageButton'
+
+export default function ImageBoxUnderHeroSection() {
+  return (
+    <div className='flex items-center justify-center'>
+        <div className='w-[340px] h-[300px] flex justify-center items-center overflow-hidden relative rounded-tl-4xl rounded-br-4xl'>
+            <Image 
+                fill={true}
+                quality={100}
+                priority
+                placeholder='blur'
+                alt="User profile picture"
+                src={"/profileImage.jpg"} 
+                blurDataURL='/images/Profile2.webp'
+            /> 
+
+            <HeroImageButton 
+                text="20+ completed projects" 
+                position="right-0 top-0" 
+                border='border-b border-l rounded-bl-xl' 
+            />
+
+            <HeroImageButton 
+                text="2 years experience" 
+                position="left-0 bottom-0" 
+                border='border-t border-r rounded-tr-xl' 
+            />
+
+        </div>
+    </div>
+  )
+}
