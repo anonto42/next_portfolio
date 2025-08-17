dd19e732e26d83d8d5f57c563cd6df7b5f29bb80
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-17 11:36:02 +0600

    skils section

---
+++ src/components/ui/section/SkillsSections.tsx
@@ -0,0 +1,23 @@
+import ClientsLayout from '@/layouts/ClientsLayout';
+import React from 'react'
+import SkillsContainer from '../Container/SkillsContainer';
+
+export default function SkillsSections() {
+
+  const header = "Design Skills That Drive Impact";
+  const subHeader = "I craft intuitive, user-friendly interfaces by combining research, creativity, and strategy — ensuring each design is both beautiful and purposeful";
+
+  return (
+    <div className='w-full h-auto text-center py-[90px]'>
+      <ClientsLayout>
+        <h1 className='text-3xl font-bold tracking-wide'>{header}</h1>
+        <p className='text-base text-[#474747da] w-1/2 mx-auto'>{subHeader}</p>
+
+        <div className='w-full h-auto flex items-center justify-center mt-10'>
+          <SkillsContainer />
+        </div>
+
+      </ClientsLayout>
+    </div>
+  )
+}
