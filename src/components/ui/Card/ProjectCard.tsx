97eefcc358f491b7e0d55da73a80fd6307a0dc8c
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-17 15:49:54 +0600

    size updated

--- src/components/ui/Card/ProjectCard.tsx
+++ src/components/ui/Card/ProjectCard.tsx
@@ -15,12 +15,12 @@ export default function ProjectCard(
     }
 ) {
   return (
-    <div key={index} className='w-[290px] h-[402px] flex flex-col items-center mx-auto bg-gray-50 rounded-xl shadow-[0_0px_10px_rgba(0,0,0,0.1)] hover:shadow-[0_0px_10px_rgba(0,0,0,0.2)] cursor-pointer transition-transform duration-200 hover:scale-[1.01]'>
-        <div className='w-[265px] h-[265px] rounded-xl bg-[#323232] relative mt-3 overflow-hidden'>
+    <div key={index} className='w-[250px] h-[350px] flex flex-col items-center mx-auto bg-gray-100 rounded-xl shadow-[0_0px_10px_rgba(0,0,0,0.1)] hover:shadow-[0_0px_10px_rgba(0,0,0,0.2)] cursor-pointer transition-transform duration-200 hover:scale-[1.01] pb-3'>
+        <div className='w-[230px] h-[230px] rounded-xl bg-[#323232] relative mt-3 overflow-hidden'>
             <Image src={image} alt={name} fill className='object-cover' />
         </div>
         <h2 className='text-xl font-medium mt-2'>{name}</h2>
-        <p className='text-sm text-gray-500 mt-1 overflow-hidden'>{description}</p>
+        <p className='text-sm text-gray-500 overflow-hidden'>{description}</p>
         <button className='hover:bg-[#1172B2] border border-[#1172B2] text-[#1172B2] hover:text-white px-4 py-2 rounded-full mt-2 hover:scale-105 transition-transform duration-300 cursor-pointer'>View Project</button>
     </div>
   )
