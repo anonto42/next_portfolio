46064d791566ed47a4f496c674063894a6198f60
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-17 18:14:07 +0600

    working on the page

--- src/app/(main)/portfolio/page.tsx
+++ src/app/(main)/portfolio/page.tsx
@@ -1,7 +1,63 @@
+import ProjectCard from '@/components/ui/Card/ProjectCard'
+import ClientsLayout from '@/layouts/ClientsLayout'
 import React from 'react'
 
 export default function PortfolioPage() {
+
+  const projects = [
+    {
+      name: "Project 1",
+      image: "/skills_images/first.png",
+      description: "Description for Project 1", 
+    },
+    {
+      name: "Project 2",
+      image: "/skills_images/second.png",
+      description: "Description for Project 2", 
+    },
+    {
+      name: "Project 3",
+      image: "/skills_images/third.png",
+      description: "Description for Project 3", 
+    },
+    {
+      name: "Project 1",
+      image: "/skills_images/first.png",
+      description: "Description for Project 1", 
+    },
+    {
+      name: "Project 2",
+      image: "/skills_images/second.png",
+      description: "Description for Project 2", 
+    },
+    {
+      name: "Project 3",
+      image: "/skills_images/third.png",
+      description: "Description for Project 3", 
+    }
+]
+
   return (
-    <div>PortfolioPage</div>
+    <div className='w-full h-auto'>
+      <ClientsLayout>
+        <div className='py-12'>
+
+          <div className='flex flex-wrap gap-4'>
+            {
+              projects.map((project, index) => (
+                <ProjectCard 
+                  key={index} 
+                  name={project.name} 
+                  image={project.image} 
+                  description={project.description} 
+                  index={index} 
+                />
+              ))
+            }
+          </div>
+        </div>
+
+      </ClientsLayout>
+    </div>
   )
 }
