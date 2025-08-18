cd534fd8248c0db5ca69668cd56562c9937f4922
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-18 08:38:28 +0600

    profile image changed

--- src/components/ui/section/ImageBoxUnderHeroSection.tsx
+++ src/components/ui/section/ImageBoxUnderHeroSection.tsx
@@ -5,14 +5,14 @@ import HeroImageButton from '../Buttons/HeroImageButton'
 export default function ImageBoxUnderHeroSection() {
   return (
     <div className='flex items-center justify-center'>
-        <div className='w-[340px] h-[300px] flex justify-center items-center overflow-hidden relative rounded-tl-4xl rounded-br-4xl'>
+        <div className='w-[340px] h-[340px] flex justify-center items-center overflow-hidden relative rounded-tl-4xl rounded-br-4xl'>
             <Image 
                 fill={true}
                 quality={100}
                 priority
                 placeholder='blur'
                 alt="User profile picture"
-                src={"/profileImage.jpg"} 
+                src={"/profileImage.webp"} 
                 blurDataURL='/images/Profile2.webp'
                 className='object-cover'
             /> 
